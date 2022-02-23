745f380aca44b563718194f3171991fbfaf974ce
Gor Yeghoyan <31503084+GorYeghoyan@users.noreply.github.com>
2022-02-23 21:08:56 +0400

    Adding Files
    Adding full Project

---
+++ Core Data Demo/Core Data Demo/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  Core Data Demo
+//
+//  Created by Gor Yeghoyan on 23.02.22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
